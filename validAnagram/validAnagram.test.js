43341605072310d845c4b8f5df782d55e6245a55
DIR\peteris.caurs <peteris.caurs@accenture.com>
2018-09-05 15:42:12 +0300

    Added test case

--- validAnagram/validAnagram.test.js
+++ validAnagram/validAnagram.test.js
@@ -8,6 +8,10 @@ test('first input is not an anagram of the second input', () => {
   expect(validAnagram('road', 'code')).toBe(false);
 });
 
+test('inputs of same length, same letters, but letter frequencies are different', () => {
+  expect(validAnagram('yellow', 'ye low')).toBe(false);
+});
+
 test('two empty inputs', () => {
   expect(validAnagram('', '')).toBe(true);
 });
