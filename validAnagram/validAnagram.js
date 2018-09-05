8ffe9f75276b381a2fe1169a21fbbc4713c25ebf
DIR\peteris.caurs <peteris.caurs@accenture.com>
2018-09-05 15:45:28 +0300

    Implemented function

--- validAnagram/validAnagram.js
+++ validAnagram/validAnagram.js
@@ -10,7 +10,31 @@ Inputs are lowercase single words (strings).
 No spaces, punctuation, numbers etc.
 */
 const validAnagram = (str1, str2) => {
-    // TODO: implement this function
+    if (str1.length !== str2.length) {
+        return false;
+    }
+    
+    const charCounter1 = {};
+    for (let char of str1) {
+        charCounter1[char] = (charCounter1[char] || 0) + 1;
+    }
+
+    const charCounter2 = {};
+    for (let char of str2) {
+        charCounter2[char] = (charCounter2[char] || 0) + 1;
+    }
+
+    for (let key in charCounter1) {
+        if (!(key in charCounter2)) {
+            return false;
+        }
+
+        if (charCounter1[key] !== charCounter2[key]) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 module.exports = validAnagram;
