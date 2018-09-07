643d26b22064acf6e9a2edde983f13c39cc4a3c6
DIR\peteris.caurs <peteris.caurs@accenture.com>
2018-09-07 12:59:54 +0300

    Examples💩👍

---
+++ problem-solving-patterns/multiple-pointers/examples/sum-zero.js
@@ -0,0 +1,24 @@
+const naiveSumZero = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === 0) {
+                return [arr[i], arr[j]];
+            }
+        }
+    }
+}
+
+const sumZero = (arr) => {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right) {
+        let sum = arr[left] + arr[right];
+        if (sum === 0) {
+            return [arr[left], arr[right]]
+        } else if (sum > 0) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+}
